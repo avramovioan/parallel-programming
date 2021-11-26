b15ea779d5dba4afd1fe1983d4c86e3bf35d8988
Ioan Avramov <avramovioan@gmail.com>
2021-11-26 14:06:25 +0200

    Removed unnecessary comment

--- ElevatorSimulator/Program.cs
+++ ElevatorSimulator/Program.cs
@@ -7,7 +7,7 @@ namespace ElevatorSimulator
         static void Main(string[] args)
         {
             // You can adjust the agent count here (while initializing the base)
-            Base b = new Base(3); // = 5 agents in the base
+            Base b = new Base(3);
             b.OpenBase();
             
         }
